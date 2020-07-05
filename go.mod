64542dd8c29bbe20498776c2350bd7e82c971d62
Álvaro Castellano Vela <alvaro.castellano.vela@gmail.com>
2020-07-05 21:27:05 +0200

    Start writting info retrieval function

--- go.mod
+++ go.mod
@@ -5,5 +5,5 @@ go 1.13
 require (
 	github.com/a-castellano/music-manager-common-types v0.0.0-20200620184725-36817c5898a6
 	github.com/a-castellano/music-manager-metal-archives-wrapper v0.0.0-20200119190308-1f9fccfc2938
-	golang.org/x/net v0.0.0-20200602114024-627f9648deb9
+	golang.org/x/net v0.0.0-20200625001655-4c5254603344
 )
--- jobs/manage_jobs.go
+++ jobs/manage_jobs.go
@@ -3,13 +3,31 @@ package jobs
 import (
 	"fmt"
 	"github.com/a-castellano/music-manager-common-types/types"
+	"net/http"
 )
 
-func ProcessJob(data []byte) error {
+func ProcessJob(data []byte, client http.Client) (bool, error) {
 	job, err := types.DecodeJob(data)
-	if err != nil {
-		return err
+	var die bool = false
+	if err == nil {
+		// Job has been successfully decoded
+		switch job.Type {
+		case types.ArtistInfoRetrieval:
+			// Data must be
+			retrievalData := types.DecodeInfoRetrieval(job.Data)
+			switch retrievalData.Type {
+			case types.ArtistName:
+				data, extraData, err := artists.SearchArtist(client, retrievalData.Artist)
+			default:
+				err = errors.New("Music Manager Metal Archives Wrapper - ArtistInfoRetrieval type should be only ArtistName.")
+			}
+		case types.RecordInfoRetrieval:
+			fmt.Println("RecordInfoRetrieval")
+		case types.JobInfoRetrieval:
+			err = errors.New("Music Manager Metal Archives Wrapper - should not receive 'Jon Info Retrieval' jobs.")
+		case types.Die:
+			die = true
+		}
 	}
-	fmt.Println(job.ID)
-	return nil
+	return err
 }
