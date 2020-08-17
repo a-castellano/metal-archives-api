4d73c7da1d3f738b44aa4ef402a6b7979ddad3cf
Álvaro Castellano Vela <alvaro.castellano.vela@gmail.com>
2020-08-17 19:25:51 +0200

    Test artist retrieval info

--- jobs/manage_jobs_test.go
+++ jobs/manage_jobs_test.go
@@ -2,6 +2,7 @@ package jobs
 
 import (
 	"bytes"
+	commontypes "github.com/a-castellano/music-manager-common-types/types"
 	"io/ioutil"
 	"net/http"
 	"testing"
@@ -17,6 +18,51 @@ func (rtm *RoundTripperMock) RoundTrip(*http.Request) (*http.Response, error) {
 }
 
 func TestProcessJobEmptyData(t *testing.T) {
+
+	var emptyData []byte
+
+	client := http.Client{Transport: &RoundTripperMock{Response: &http.Response{Body: ioutil.NopCloser(bytes.NewBufferString(`
+{
+	"error": "",
+	"iTotalRecords": 3,
+	"iTotalDisplayRecords": 3,
+	"sEcho": 0,
+	"aaData": [
+				[
+			"<a href=\"https://www.metal-archives.com/bands/Burzum/88\">Burzum</a>  <!-- 11.432714 -->" ,
+			"Black Metal, Ambient" ,
+			"Norway"     		]
+				,
+						[
+			"<a href=\"https://www.metal-archives.com/bands/Down_to_Burzum/3540435931\">Down to Burzum</a>  <!-- 5.716357 -->" ,
+			"Black Metal" ,
+			"Brazil"     		]
+				,
+						[
+			"<a href=\"https://www.metal-archives.com/bands/Krimparturr/21151\">Krimparturr</a> (<strong>a.k.a.</strong> Krimpartûrr Bürzum Shi-Hai) <!-- 1.2505064 -->" ,
+			"Black Metal" ,
+			"Brazil"     		]
+				]
+}
+	`))}}}
+
+	die, err := ProcessJob(emptyData, client)
+
+	if err.Error() != "Empty data received." {
+		t.Errorf("Message with failed data should return 'Empty data received.' error, not '%s'.", err.Error())
+	}
+
+	if die == true {
+		t.Errorf("Message with failed data does not stop this service.")
+	}
+}
+
+func TestProcessJobOneArtist(t *testing.T) {
+
+	var infoRetrieval commontypes.InfoRetrieval
+
+	infoRetrieval.Type = commontypes.ArtistName
+	infoRetrieval.Artist = "Burzum"
 
 	var emptyData []byte
 
