10efd82988858e2826a0f9cf7857baa7a80a95fd
Álvaro Castellano Vela <alvaro.castellano.vela@gmail.com>
2020-08-30 16:45:43 +0000

    ReadConfig method without no server config should fail.

--- config_reader/read_config.go
+++ config_reader/read_config.go
@@ -32,9 +32,8 @@ func ReadConfig() (Config, error) {
 	var config Config
 
 	//Look for config file location defined as env var
-
+	viper.BindEnv("MUSIC_MANAGER_METAL_ARCHIVES_WRAPPER_CONFIG_FILE_LOCATION")
 	configFileLocation = viper.GetString("MUSIC_MANAGER_METAL_ARCHIVES_WRAPPER_CONFIG_FILE_LOCATION")
-	//	no_variable := configFileLocation
 	if configFileLocation == "" {
 		// Get config file from default location
 		configFileLocation = "/etc/music-manager-metal-archives-wrapper/"
@@ -42,8 +41,8 @@ func ReadConfig() (Config, error) {
 	viper.SetConfigName("config")
 	viper.SetConfigType("toml")
 	viper.AddConfigPath(configFileLocation)
-	err := viper.ReadInConfig()
-	if err != nil {
+
+	if err := viper.ReadInConfig(); err != nil {
 		return config, errors.New(errors.New("Fatal error config file: ").Error() + err.Error())
 	}
 
--- config_reader/read_config_test.go
+++ config_reader/read_config_test.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"os"
 	"testing"
 )
 
@@ -12,6 +13,20 @@ func TestProcessNoConfigFilePresent(t *testing.T) {
 	} else {
 		if err.Error() != "Fatal error config file: Config File \"config\" Not Found in \"[/etc/music-manager-metal-archives-wrapper]\"" {
 			t.Errorf("Default config should be in /etc/music-manager-metal-archives-wrapper/config.toml, not in other place, error was '%s'.", err.Error())
+		}
+	}
+
+}
+
+func TestProcessNoServerInConfig(t *testing.T) {
+
+	os.Setenv("MUSIC_MANAGER_METAL_ARCHIVES_WRAPPER_CONFIG_FILE_LOCATION", "./config_files_test/no_server/")
+	_, err := ReadConfig()
+	if err == nil {
+		t.Errorf("ReadConfig method without no server config should fail.")
+	} else {
+		if err.Error() != "Fatal error config: no server config was found." {
+			t.Errorf("Error should be \"Fatal error config: no server config was found.\" but error was '%s'.", err.Error())
 		}
 	}
 
